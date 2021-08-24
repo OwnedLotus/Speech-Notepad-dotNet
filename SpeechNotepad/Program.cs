4384a3e4faa5280c49d73f318bf4ccdfa014f4ba
Jonah Ringdahl <JonahRingdahl@outlook.com>
2021-08-23 20:29:50 -0400

    Building out the codebase
    Finding good ways to run the Speech Recognition.

--- SpeechNotepad/Program.cs
+++ SpeechNotepad/Program.cs
@@ -1,45 +1,83 @@
-﻿using System;
+﻿using System.Speech;
 using System.Speech.Recognition;
+using System.Speech.Synthesis;
 using System.Threading;
 
-namespace SpeechNotepad
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("windows10.0.17763.0")]
+
+string[] userInput = null;
+
+string word = null;
+
+Choices choices = new Choices(new string[] { "Start", "Testing", "Save", "Quit", "Read" , "Repeat" });
+SpeechRecognitionEngine engine = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+Grammar grammar = new Grammar(new GrammarBuilder (choices));
+
+engine.LoadGrammarAsync(grammar);
+
+engine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Engine_SpeechRecognized);
+
+engine.SetInputToDefaultAudioDevice();
+
+engine.RecognizeAsync(RecognizeMode.Multiple);
+
+
+while (true)
 {
-    class Program
+    Console.ReadLine();
+}
+
+
+
+
+
+static void Engine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+{
+    Console.WriteLine($"Recognized text: {e.Result.Text}");
+
+    switch (e.Result.Text)
     {
-        static ManualResetEvent completed = null;
+        case "Start":
+            Console.WriteLine("Turning on Notepad Speech Recognition....");
+            StartNotepad();
+            break;
+        case "Testing":
+            Console.WriteLine($"Testing Successfull!");
+            break;
+        case "Quit":
+            Console.WriteLine("Quitting....");
+            Thread.Sleep(1000);
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine(e.Result.Text);
+            break;
+    }
+}
 
-        static void Main(string[] args)
+
+static void StartNotepad()
+{
+    using (SpeechRecognitionEngine recognizer =
+        new SpeechRecognitionEngine(
+            new System.Globalization.CultureInfo("en-US")))
+    {
+        recognizer.LoadGrammar(new DictationGrammar());
+
+        recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Recognizer_SpeechRecognized);
+
+        recognizer.SetInputToDefaultAudioDevice();
+
+        recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+        while (true)
         {
-            Choices choices = new Choices(new string[] { "Testing" });
-            completed = new ManualResetEvent(false);    
-            
-            SpeechRecognitionEngine speechRecognitionEngine = new SpeechRecognitionEngine();
-            speechRecognitionEngine.LoadGrammarAsync(new Grammar( new GrammarBuilder(choices) ) );
-            speechRecognitionEngine.SpeechRecognized += SpeechRecognitionEngine_SpeechRecognized;
-
-            speechRecognitionEngine.SetInputToDefaultAudioDevice();
-            speechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
-
-            completed.WaitOne();
-            speechRecognitionEngine.Dispose();
-                
-            
-        }
-
-        private static void SpeechRecognitionEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
-        {
-            if (e.Result.Text == "Testing")
-            {
-                Console.WriteLine("Testing Successful");
-            }
-            else if(e.Result.Text == "Hello")
-            {
-                Console.WriteLine("Hello World");
-            }
-            else
-            {
-                Console.WriteLine("Speech Not Recognized");
-            }
+            Console.ReadLine();
         }
     }
+}
+
+static void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+{ 
+
 }
