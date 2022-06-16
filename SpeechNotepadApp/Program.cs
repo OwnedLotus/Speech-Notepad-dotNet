bbabf8a73ae0b843987100d3ab3dbee80397fe02
Jonah Ringdahl <46978061+OwnedLotus@users.noreply.github.com>
2022-06-16 19:08:16 -0400

    Removing Top Level Statement sturcture

---
+++ SpeechNotepadApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Speech;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Threading;
+
+namespace SpeechNotepadApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
